57ff9e6b027b0ad05836b4669dfe969ed0b25740
Jacek Smolak <jacek.smolak@gmail.com>
2023-04-27 23:29:58 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
@@ -39,4 +40,5 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"],
     },
   },
+  adapter: vercel()
 });
